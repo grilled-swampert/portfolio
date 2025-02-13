75e7f84dcc8d83bcc9455a645d53b801c0bbe2bd
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2025-02-14 02:03:54 +0530

    frontend changes

--- src/components/Warfare.tsx
+++ src/components/Warfare.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-
 const Warfare = () => {
   return (
-    <div className="bg-black w-full h-[100vh] flex flex-col items-center justify-center">
-      yuio
+    <div className="bg-black w-full h-[100vh] text-white flex flex-col items-center justify-center">
+        <div>
+          <div style={{ fontFamily: "afacad-bold" }}>I LOST ALL FOCUS</div>
+          <div style={{ fontFamily: "dynapuff-regular" }}>BLURRING</div>
+          <div style={{ fontFamily: "afacad-bold" }}>THROUGH THESE MOTIONS</div>
+        </div>
+        <div>
+          <div style={{ fontFamily: "poppins-bold" }}>
+            YOU DON'T GIVE A F*CK SINCE
+          </div>
+          <div style={{ fontFamily: "afacad-bold" }}>DISTANCE TURNED TO</div>
+          <div style={{ fontFamily: "afacad-regular" }}>OCEANS</div>
+        </div>
     </div>
   );
 };
